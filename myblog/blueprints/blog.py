043911d99544af684ce5c53a850f8129bea06fbd
Administrator <52603245@qq.com>
2020-06-11 16:42:16 +0800

    2020.6.11下午

--- myblog/blueprints/blog.py
+++ myblog/blueprints/blog.py
@@ -1,12 +1,15 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,current_app
 from myblog.models import Post
 
 blog_bp=Blueprint('blog',__name__)
 
 @blog_bp.route('/')
 def index():
-    posts=Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('blog/index.html',posts=posts)
+    page=request.args.get('page',1,type=int)  # 从查询字符串获取当前页数
+    per_page=current_app.config['MYBLOG_POST_PER_PAGE']  # 每页数量
+    pagination=Post.query.order_by(Post.timestamp.desc()).paginate(page,per_page=per_page) # 分页对象
+    posts=pagination.items  # 当前页数的记录页表
+    return render_template('blog/index.html',pagination=pagination,posts=posts)
 
 @blog_bp.route('/about')
 def about():
@@ -16,6 +19,7 @@ def about():
 def show_category(category_id):
     return render_template('blog/category.html')
 
-@blog_bp.route('/post/<int:post_id>',method=['GET','POST'])
+@blog_bp.route('/post/<int:post_id>')
 def show_post(post_id):
+    post=Post.query.get_or_404(post_id)
     return render_template('blog/post.html')
