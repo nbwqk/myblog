7b4db1dd28d9821d4f3b697c640238a21aca4265
nbwqk <52603245@qq.com>
2020-06-12 22:04:55 +0800

    2020.6.12night

--- myblog/blueprints/blog.py
+++ myblog/blueprints/blog.py
@@ -1,5 +1,8 @@
-from flask import Blueprint,render_template,request,current_app
+from flask import Blueprint,render_template,request,current_app,url_for,flash,redirect
+from flask_login import current_user
 from myblog.models import Post,Category,Comment
+from myblog.forms import AdminCommentForm,CommentForm
+from myblog.emails import send_new_reply_email,send_new_comment_email
 
 blog_bp=Blueprint('blog',__name__)
 
@@ -32,4 +35,38 @@ def show_post(post_id):
     pagination=Comment.query.with_parent(post).filter_by(reviewed=True).order_by(Comment.timestamp.asc()).paginate(
       page,per_page)
     comments=pagination.items
-    return render_template('blog/post.html',post=post,pagination=pagination,comments=comments)
+
+    if current_user.is_authenticated:
+        form=AdminCommentForm()
+        form.author.data=current_user.name
+        form.email.data=current_user.config['MYBLOG_EMAIL']
+        form.site.data=url_for('.index')
+        from_admin=True
+        reviewed=True
+    else:
+        form=CommentForm()
+        from_admin=False
+        reviewed=False
+
+    if form.validate_on_submit():
+        author=form.author.data
+        email=form.email.data
+        site=form.site.data
+        body=form.body.data
+        comment=Comment(
+            author=author,email=email,site=site,body=body,
+            from_admin=from_admin,post=post,reviewed=reviewed)
+        replied_id=request.args.get('reply')
+        if replied_id:
+            replied_comment=Comment.query.get_or_404(replied_id)
+            comment.replied=replied_comment
+            send_new_reply_email(replied_comment)
+        db.session.add(comment)
+        db.session.commit()
+        if current_user.is_authenticated:
+            flash('Comment publish.','success')
+        else:
+            flash('Thanks,your comment will be published after reviewed.','info')
+            send_new_comment_email(post)
+        return redirect(url_for('.show_post',post_id=post_id))
+    return render_template('blog/post.html',post=post,pagination=pagination,form=form,comments=comments)
