1279821eaaea845450dbe9cc398a30a27849a8d6
Administrator <52603245@qq.com>
2020-06-12 16:35:29 +0800

    2020.6.12下午

--- myblog/blueprints/blog.py
+++ myblog/blueprints/blog.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,render_template,request,current_app
-from myblog.models import Post
+from myblog.models import Post,Category,Comment
 
 blog_bp=Blueprint('blog',__name__)
 
@@ -17,9 +17,19 @@ def about():
 
 @blog_bp.route('/category/<int:category_id>')
 def show_category(category_id):
-    return render_template('blog/category.html')
+    category=Category.query.get_or_404(category_id)
+    page=request.args.get('page',1,type=int)
+    per_page=current_app.config['MYBLOG_POST_PER-PAGE']
+    pagination=Post.query.with_parent(category).order_by(Post.timestamp.desc()).paginate(page,per_page)
+    posts=pagination.items
+    return render_template('blog/category.html',category=category,pagination=pagination,posts=posts)
 
 @blog_bp.route('/post/<int:post_id>')
 def show_post(post_id):
     post=Post.query.get_or_404(post_id)
-    return render_template('blog/post.html')
+    page=request.args.get('page',1,type=int)
+    per_page=current_app.config['MYBOLG_COMMENT_PER_PAGE']
+    pagination=Comment.query.with_parent(post).filter_by(reviewed=True).order_by(Comment.timestamp.asc()).paginate(
+      page,per_page)
+    comments=pagination.items
+    return render_template('blog/post.html',post=post,pagination=pagination,comments=comments)
