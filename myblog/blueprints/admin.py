48529137703ee7ebfb741bf676c7095d0c80cb74
nbwqk <52603245@qq.com>
2020-06-15 22:31:08 +0800

    2020.6.15night

--- myblog/blueprints/admin.py
+++ myblog/blueprints/admin.py
@@ -1,3 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint,flash
+from flask_login import login_required
+from myblog.models import Post
+from myblog.utils import redirect_back
+from myblog.extensions import db
 
 admin_bp=Blueprint('admin',__name__)
+
+@admin_bp.route('/post/<int:post_id>/delete',methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post=Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post delete.','success')
+    return redirect_back()
+
+
--- myblog/extensions.py
+++ myblog/extensions.py
@@ -2,9 +2,10 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_ckeditor import CKEditor
-from flask_moment import  Moment
+from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_wtf import CSRFProtect
 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
@@ -13,9 +14,13 @@ ckeditor=CKEditor()
 mail=Mail()
 migrate=Migrate()
 login_manager=LoginManager()
+csrf=CSRFProtect()
 
 @login_manager.user_loader
 def load_user(user_id):
     from myblog.models import Admin
     user=Admin.query.get(int(user_id))
     return user
+
+login_manager.login_view='auth.login'
+login_manager.login_message_category='warning'
